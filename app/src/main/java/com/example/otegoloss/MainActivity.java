199b7ea2f735db8d6f2c04b5ea59eb7de87fa141
mayachos <amacrk@icloud.com>
2021-12-31 17:59:38 +0900

    バグ修正

--- app/src/main/java/com/example/otegoloss/MainActivity.java
+++ app/src/main/java/com/example/otegoloss/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.otegoloss;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 import com.example.otegoloss.databinding.ActivityMainBinding;
@@ -28,7 +29,9 @@ public class MainActivity extends AppCompatActivity {
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.navigation_home, R.id.navigation_mypage, R.id.navigation_shipping, R.id.navigation_user)
                 .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.nav_host_fragment_activity_main);
+        NavController navController = navHostFragment.getNavController();
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
     }
