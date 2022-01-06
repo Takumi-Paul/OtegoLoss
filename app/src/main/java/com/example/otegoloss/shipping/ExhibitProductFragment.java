fa1bc1117b82dc584332c9005eca61cb3a8feff6
Takumi-Paul <230369t@ugs.kochi-tech.ac.jp>
2022-01-06 18:45:37 +0900

    fragment_shipping.xmlを編集。出品履歴画面へ移動するようにした

---
+++ app/src/main/java/com/example/otegoloss/shipping/ExhibitProductFragment.java
@@ -0,0 +1,17 @@
+/*
+出品関連、出品情報入力画面の機能
+ */
+package com.example.otegoloss.shipping;
+
+import android.os.Bundle;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.otegoloss.R;
+
+
+public class ExhibitProductFragment {
+}
