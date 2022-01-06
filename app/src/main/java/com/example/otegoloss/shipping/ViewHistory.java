b2862b9aca0214204e6a1c1af91bb04e7c1af257
Takumi-Paul <230369t@ugs.kochi-tech.ac.jp>
2022-01-06 14:32:22 +0900

    ViewHistory.java（出品履歴機能）にpublic void viewYetSoldOutHistory(View view)メソッドとprotected void onCreate(Bundle savedInstanceState) メソッドを追加

--- app/src/main/java/com/example/otegoloss/shipping/ViewHistory.java
+++ app/src/main/java/com/example/otegoloss/shipping/ViewHistory.java
@@ -1,4 +1,27 @@
 package com.example.otegoloss.shipping;
 
-public class ViewHistory {
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.content.Intent;
+
+import com.example.otegoloss.R;
+
+public class ViewHistory extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_view_yet_sold_out_history);
+    }
+
+    public void viewYetSoldOutHistory(View view) {
+
+    }
+
+
+
 }
