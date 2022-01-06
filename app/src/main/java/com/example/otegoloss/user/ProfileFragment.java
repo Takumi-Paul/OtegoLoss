0880b11d39c8310dec43f4b43f6401ff1b5ed852
gimmical <230301e@ugs.kochi-tech.ac.jp>
2022-01-06 17:33:36 +0900

    test

---
+++ app/src/main/java/com/example/otegoloss/user/ProfileFragment.java
@@ -0,0 +1,12 @@
+package com.example.otegoloss.user;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.content.Intent;
+
+public class ProfileFragment {
+}
