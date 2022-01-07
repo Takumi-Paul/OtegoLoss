aabe32915566b1e16307dc9d2246873c8a9206b3
KawakamiShoya <230318x@ugs.kochi-tech.ac.jp>
2022-01-07 16:34:01 +0900

    いろいろ追加しました　川上

---
+++ app/src/main/java/com/example/otegoloss/home/ViewProduct.java
@@ -0,0 +1,29 @@
+/*
+12/29
+Kobayashi
+ホーム画面を生成するプログラム
+ */
+
+package com.example.otegoloss.home;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.otegoloss.R;
+
+public class ViewProduct extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+
+        View view = inflater.inflate(R.layout.fragment_view_product, container, false);
+        return view;
+    }
+
+}
---
+++ app/src/main/java/com/example/otegoloss/home/ViewSearch.java
@@ -0,0 +1,28 @@
+/*
+12/29
+Kobayashi
+ホーム画面を生成するプログラム
+ */
+
+package com.example.otegoloss.home;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.otegoloss.R;
+
+public class ViewSearch extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_view_search, container, false);
+        return view;
+    }
+
+}
