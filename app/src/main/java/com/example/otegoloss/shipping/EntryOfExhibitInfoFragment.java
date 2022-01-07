cf9b9fce1a6c4b3f62a2579a39476c8da1a64706
Takumi-Paul <230369t@ugs.kochi-tech.ac.jp>
2022-01-07 23:30:42 +0900

    ViewYetSoldOutHistoryFragmentで画面が表示されるように変更

--- app/src/main/java/com/example/otegoloss/shipping/EntryOfExhibitInfoFragment.java
+++ app/src/main/java/com/example/otegoloss/shipping/EntryOfExhibitInfoFragment.java
@@ -8,10 +8,29 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
+import com.example.otegoloss.MainActivity;
 import com.example.otegoloss.R;
 
 
 public class EntryOfExhibitInfoFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // フラグメントで表示する画面をlayoutファイルからインフレートする
+        View view = inflater.inflate(R.layout.fragment_entry_of_exhibit_info, container, false);
+
+        // 所属している親アクティビティを取得
+        MainActivity activity = (MainActivity) getActivity();
+        // アクションバーにタイトルをセット
+        activity.setTitle("出品情報入力");
+        // 戻るボタンは非表示にする（MainFragmentでは戻るボタン不要）
+        // ここをfalseにしておかないとサブフラグメントから戻ってきた際に戻るボタンが表示されたままになってしまう
+        activity.setupBackButton(true);
+
+        return view;
+    }
+
 
 }
--- app/src/main/java/com/example/otegoloss/shipping/ViewYetSoldOutHistoryFragment.java
+++ app/src/main/java/com/example/otegoloss/shipping/ViewYetSoldOutHistoryFragment.java
@@ -3,24 +3,62 @@ package com.example.otegoloss.shipping;
 
 import android.os.Bundle;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import com.example.otegoloss.MainActivity;
 import com.example.otegoloss.R;
-
+import com.example.otegoloss.user.ProfileConfigFragment;
 
 
 public class ViewYetSoldOutHistoryFragment extends Fragment {
     //画面表示
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // フラグメントで表示する画面をlayoutファイルからインフレートする
+        View view = inflater.inflate(R.layout.fragment_view_yet_sold_out_history, container, false);
 
+        // 所属している親アクティビティを取得
+        MainActivity activity = (MainActivity) getActivity();
+        // アクションバーにタイトルをセット
+        activity.setTitle("出品履歴");
+        // 戻るボタンは非表示にする（MainFragmentでは戻るボタン不要）
+        // ここをfalseにしておかないとサブフラグメントから戻ってきた際に戻るボタンが表示されたままになってしまう
+        activity.setupBackButton(true);
 
-        return inflater.inflate(R.layout.fragment_view_yet_sold_out_history, container, false);
+        // ボタン要素を取得
+        Button bt1 = view.findViewById(R.id.button_soldout_sold_out_history);
+
+        // 完売済みボタンをクリックした時の処理
+        bt1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // fragment_view_sold_out_historyに遷移させる
+                replaceFragment1(new ShippingFragment());
+            }
+        });
+
+        return view;
     }
 
+    // 表示させるFragmentを切り替えるメソッドを定義（表示したいFragmentを引数として渡す）
+    //ShippingFragmentへの移動
+    private void replaceFragment1(Fragment fragment) {
+        // フラグメントマネージャーの取得
+        FragmentManager manager = getFragmentManager(); // アクティビティではgetSupportFragmentManager()?
+        // フラグメントトランザクションの開始
+        FragmentTransaction transaction = manager.beginTransaction();
+        // レイアウトをfragmentに置き換え（追加）
+        transaction.replace(R.id.viewYetSoldOutHistory, fragment);
+        // 置き換えのトランザクションをバックスタックに保存する
+        transaction.addToBackStack(null);
+        // フラグメントトランザクションをコミット
+        transaction.commit();
+    }
 
 }
