f27e34d8c65f885859db71f850de5658ef4dc309
gimmical <230301e@ugs.kochi-tech.ac.jp>
2022-01-07 23:46:06 +0900

    ホーム画面UI修正

--- app/src/main/java/com/example/otegoloss/home/HomeFragment.java
+++ app/src/main/java/com/example/otegoloss/home/HomeFragment.java
@@ -7,20 +7,18 @@ Kobayashi
 package com.example.otegoloss.home;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
 
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
 import com.example.otegoloss.MainActivity;
 import com.example.otegoloss.R;
 import com.example.otegoloss.user.ProfileConfigFragment;
-import com.google.firebase.inappmessaging.model.Button;
 
 public class HomeFragment extends Fragment {
 
