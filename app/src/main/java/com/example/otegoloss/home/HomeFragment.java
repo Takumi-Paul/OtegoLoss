2488971eda3f224573fc8949ad89ed40343d6fbe
mayachos <amacrk@icloud.com>
2022-01-08 13:00:35 +0900

    Home画面のgridViewの作成

--- app/src/main/java/com/example/otegoloss/home/HomeFragment.java
+++ app/src/main/java/com/example/otegoloss/home/HomeFragment.java
@@ -6,11 +6,14 @@ Kobayashi
 
 package com.example.otegoloss.home;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
+import android.widget.GridView;
+import com.example.otegoloss.GridAdapter;
 
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
@@ -20,48 +23,44 @@ import com.example.otegoloss.MainActivity;
 import com.example.otegoloss.R;
 import com.example.otegoloss.user.ProfileConfigFragment;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class HomeFragment extends Fragment {
+
+    private String[] productNames = {
+            "tomato", "carrot"
+    };
+
+    private List<Integer> imgList = new ArrayList<>();
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
         // フラグメントで表示する画面をlayoutファイルからインフレートする
         View view = inflater.inflate(R.layout.fragment_home, container, false);
 
-        // 所属している親アクティビティを取得
-        MainActivity activity = (MainActivity) getActivity();
-        // アクションバーにタイトルをセット
-        activity.setTitle("ホーム");
-        // 戻るボタンは非表示にする（MainFragmentでは戻るボタン不要）
-        // ここをfalseにしておかないとサブフラグメントから戻ってきた際に戻るボタンが表示されたままになってしまう
-        activity.setupBackButton(false);
+        // for-each member名をR.drawable.名前としてintに変換してarrayに登録
+        for (String productName: productNames){
+            int imageId = getResources().getIdentifier(productName,"drawable", getActivity().getPackageName());
+            imgList.add(imageId);
+        }
 
-        // ボタン要素を取得
-        ImageButton bt1 = view.findViewById(R.id.productImage_imageView_home1); //R.idを修正する必要あり
+        // GridViewのインスタンスを生成
+        GridView gridview = view.findViewById(R.id.product_gridView);
+        // BaseAdapter を継承したGridAdapterのインスタンスを生成
+        // 子要素のレイアウトファイル grid_items.xml を
+        // activity_main.xml に inflate するためにGridAdapterに引数として渡す
+        GridAdapter adapter = new GridAdapter(getActivity().getApplicationContext(),
+                R.layout.grid_items,
+                imgList,
+                productNames
+        );
+        gridview.setAdapter(adapter);
 
-        // ①ボタンをクリックした時の処理
-        bt1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // SubFragment1に遷移させる
-                replaceFragment(new ProfileConfigFragment());
-            }
-        });
 
         return view;
     }
 
-    // 表示させるFragmentを切り替えるメソッドを定義（表示したいFragmentを引数として渡す）
-    private void replaceFragment(Fragment fragment) {
-        // フラグメントマネージャーの取得
-        FragmentManager manager = getFragmentManager(); // アクティビティではgetSupportFragmentManager()?
-        // フラグメントトランザクションの開始
-        FragmentTransaction transaction = manager.beginTransaction();
-        // レイアウトをfragmentに置き換え（追加）
-        transaction.replace(R.id.fragmentHome, fragment);
-        // 置き換えのトランザクションをバックスタックに保存する
-        transaction.addToBackStack(null);
-        // フラグメントトランザクションをコミット
-        transaction.commit();
-    }
 
 }
