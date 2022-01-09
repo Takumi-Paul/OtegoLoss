1586f6527145fa5aadb9be2fd576ed6ca14b0e00
KawakamiShoya <230318x@ugs.kochi-tech.ac.jp>
2022-01-09 22:57:57 +0900

    商品詳細画面から購入確認画面・コメント画面へのnavigetionを追加しました　川上

---
+++ app/src/main/java/com/example/otegoloss/home/ViewComment.java
@@ -0,0 +1,32 @@
+package com.example.otegoloss.home;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.otegoloss.MainActivity;
+import com.example.otegoloss.R;
+
+public class ViewComment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // フラグメントで表示する画面をlayoutファイルからインフレートする
+        View view = inflater.inflate(R.layout.fragment_view_comment, container, false);
+
+        // 所属親アクティビティを取得
+        MainActivity activity = (MainActivity) getActivity();
+        // 戻るボタンを表示する
+        activity.setupBackButton(true);
+
+        // この記述でフラグメントでアクションバーメニューが使えるようになる
+        setHasOptionsMenu(true);
+
+        return view;
+    }
+
+}
