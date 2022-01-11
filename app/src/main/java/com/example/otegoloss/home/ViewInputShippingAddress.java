55e3b56320682146f12399d19ef547b63cad2f2f
KawakamiShoya <230318x@ugs.kochi-tech.ac.jp>
2022-01-11 17:17:51 +0900

    購入機能に関する処理を追加しました　川上

--- app/src/main/java/com/example/otegoloss/home/ViewInputShippingAddress.java
+++ app/src/main/java/com/example/otegoloss/home/ViewInputShippingAddress.java
@@ -10,8 +10,11 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.example.otegoloss.R;
 
@@ -21,7 +24,31 @@ public class ViewInputShippingAddress extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
+
         View view = inflater.inflate(R.layout.fragment_view_input_shipping_address, container, false);
+
+
+        //「次へ」ボタンが押された時の処理
+        Button NextButtonShipping = view.findViewById(R.id.next_button_shipping);
+
+        NextButtonShipping.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Navigation.findNavController(view).navigate(R.id.action_fragmentProduct_to_purchase_information1);
+
+                FragmentManager fm_NextButtonShipping = getChildFragmentManager();
+                FragmentTransaction t_NextButtonShipping  =  fm_NextButtonShipping.beginTransaction();
+                // 次のFragment
+                Fragment secondFragment = new ViewInputPayment();
+                // fragmentManagerに次のfragmentを追加
+                t_NextButtonShipping.add(R.id.fragmentViewInputShippingAddress, secondFragment);
+                // 画面遷移戻りを設定
+                t_NextButtonShipping.addToBackStack(null);
+                // 画面遷移
+                t_NextButtonShipping.commit();
+            }
+        });
+
         return view;
     }
 
