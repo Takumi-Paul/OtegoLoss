6944b512f6c47a40c3528d952c1c9000ceb38c33
KawakamiShoya <230318x@ugs.kochi-tech.ac.jp>
2022-01-13 02:39:36 +0900

    画面遷移のプログラムを追加・修正しました　川上

--- app/src/main/java/com/example/otegoloss/home/ViewPurchaseCompleted.java
+++ app/src/main/java/com/example/otegoloss/home/ViewPurchaseCompleted.java
@@ -10,8 +10,12 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.navigation.Navigation;
 
 import com.example.otegoloss.R;
 
@@ -22,6 +26,29 @@ public class ViewPurchaseCompleted extends Fragment {
                              Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.fragment_view_purchase_completed, container, false);
+
+
+        //「次へ」ボタンが押された時の処理
+        Button BackProductButton = view.findViewById(R.id.backProduct_button);
+
+        BackProductButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Navigation.findNavController(view).navigate(R.id.action_fragmentViewPurchaseCompleted_to_fragmentProduct);
+
+                //FragmentManager fm_ViewPurchaseInformation1 = getParentFragmentManager();
+                //FragmentTransaction t_ViewPurchaseInformation1  =  fm_ViewPurchaseInformation1.beginTransaction();
+                // 次のFragment
+                //Fragment secondFragment = new ViewInputShippingAddress();
+                // fragmentManagerに次のfragmentを追加
+                //t_ViewPurchaseInformation1.replace(R.id.fragmentViewInputPayment, secondFragment);
+                // 画面遷移戻りを設定
+                //t_ViewPurchaseInformation1.addToBackStack(null);
+                // 画面遷移
+                //t_ViewPurchaseInformation1.commit();
+            }
+        });
+
         return view;
     }
 
--- app/src/main/java/com/example/otegoloss/home/ViewPurchaseInformation1.java
+++ app/src/main/java/com/example/otegoloss/home/ViewPurchaseInformation1.java
@@ -1,34 +1,53 @@
+/*
+12/29
+Kobayashi
+ホーム画面を生成するプログラム
+ */
+
 package com.example.otegoloss.home;
 
 import android.os.Bundle;
-import androidx.fragment.app.Fragment;
-
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
-import com.example.otegoloss.MainActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.navigation.Navigation;
+
 import com.example.otegoloss.R;
-import com.example.otegoloss.databinding.FragmentUserBinding;
-
-import java.util.Objects;
 
 public class ViewPurchaseInformation1 extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // フラグメントで表示する画面をlayoutファイルからインフレートする
+
         View view = inflater.inflate(R.layout.fragment_view_purchase_information1, container, false);
 
-        // 所属親アクティビティを取得
-        MainActivity activity = (MainActivity) getActivity();
-        // 戻るボタンを表示する
-        activity.setupBackButton(true);
 
-        // この記述でフラグメントでアクションバーメニューが使えるようになる
-        setHasOptionsMenu(true);
+        //「次へ」ボタンが押された時の処理
+        Button ConfirmBuyButton = view.findViewById(R.id.confirmBuy_button);
+
+        ConfirmBuyButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Navigation.findNavController(view).navigate(R.id.action_purchase_information1_to_fragmentViewPurchaseCompleted);
+
+                //FragmentManager fm_ViewPurchaseInformation1 = getParentFragmentManager();
+                //FragmentTransaction t_ViewPurchaseInformation1  =  fm_ViewPurchaseInformation1.beginTransaction();
+                // 次のFragment
+                //Fragment secondFragment = new ViewInputShippingAddress();
+                // fragmentManagerに次のfragmentを追加
+                //t_ViewPurchaseInformation1.replace(R.id.fragmentViewInputPayment, secondFragment);
+                // 画面遷移戻りを設定
+                //t_ViewPurchaseInformation1.addToBackStack(null);
+                // 画面遷移
+                //t_ViewPurchaseInformation1.commit();
+            }
+        });
 
         return view;
     }
