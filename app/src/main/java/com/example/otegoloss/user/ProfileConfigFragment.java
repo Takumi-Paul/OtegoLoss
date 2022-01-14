9c585e573b03b349202774bee2b4a2ca683cf670
gimmical <230301e@ugs.kochi-tech.ac.jp>
2022-01-14 09:57:43 +0900

    ユーザ プロフィール 入力

--- app/src/main/java/com/example/otegoloss/user/ProfileConfigFragment.java
+++ app/src/main/java/com/example/otegoloss/user/ProfileConfigFragment.java
@@ -2,6 +2,8 @@ package com.example.otegoloss.user;
 
 import android.os.Bundle;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.navigation.Navigation;
 
 
@@ -11,6 +13,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.otegoloss.MainActivity;
 import com.example.otegoloss.R;
@@ -19,6 +22,8 @@ import com.example.otegoloss.databinding.FragmentUserBinding;
 import java.util.Objects;
 
 public class ProfileConfigFragment extends Fragment {
+
+    private EditText profile_username;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -34,11 +39,14 @@ public class ProfileConfigFragment extends Fragment {
         // この記述でフラグメントでアクションバーメニューが使えるようになる
         setHasOptionsMenu(true);
 
+        profile_username = (EditText)view.findViewById(R.id.userName_editText) ;
+
         Button inputCompleteButton = view.findViewById(R.id.inputComplete_button);
         inputCompleteButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                ;
+                String profile_usernames = profile_username.getText().toString();
+                Navigation.findNavController(view).navigate(R.id.action_profile_config_to_navigation_user);
             }
         });
 
