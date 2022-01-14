62b53d93b1bcb92175e6c7f2f27c198f43d1bb17
Takumi-Paul <230369t@ugs.kochi-tech.ac.jp>
2022-01-14 14:09:53 +0900

    出品履歴周りの画面遷移を追加

--- app/src/main/java/com/example/otegoloss/shipping/ShippingProcedureFragment.java
+++ app/src/main/java/com/example/otegoloss/shipping/ShippingProcedureFragment.java
@@ -1,4 +1,3 @@
-//出品商品詳細（完売済み）
 package com.example.otegoloss.shipping;
 
 import android.os.Bundle;
@@ -11,35 +10,23 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.EditText;
 
 import com.example.otegoloss.MainActivity;
 import com.example.otegoloss.R;
 
-
-public class ViewSoldOutProductFragment extends Fragment{
+public class ShippingProcedureFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // フラグメントで表示する画面をlayoutファイルからインフレートする
-        View view = inflater.inflate(R.layout.fragment_view_sold_out_product, container, false);
+        View view = inflater.inflate(R.layout.fragment_view_shipping_procedure, container, false);
 
         // 所属している親アクティビティを取得
         MainActivity activity = (MainActivity) getActivity();
         // アクションバーにタイトルをセット
-        activity.setTitle("出品情報詳細");
+        activity.setTitle("配送手続き");
         // 戻るボタンは表示
         activity.setupBackButton(true);
 
-        // ボタンを取得
-        Button nextButton = view.findViewById(R.id.delivery_procedure_button_view_sold_out_product);
-        // 配達ボタンをクリックした時の処理
-        nextButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // fragmentDeriveryに遷移させる
-                Navigation.findNavController(view).navigate(R.id.action_navigation_sold_out_product_to_navigation_shipping_procedure);
-            }
-         });
         return view;
     }
 }
