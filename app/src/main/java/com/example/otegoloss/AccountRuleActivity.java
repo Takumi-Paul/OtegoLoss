b01141b92f38bf8ba904e0c03360488e6496ecf3
mayachos <amacrk@icloud.com>
2022-01-20 14:42:21 +0900

    ログイン画面追加(動きはまだ)

---
+++ app/src/main/java/com/example/otegoloss/AccountRuleActivity.java
@@ -0,0 +1,16 @@
+package com.example.otegoloss;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AccountRuleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_account_register);
+
+
+    }
+}
---
+++ app/src/main/java/com/example/otegoloss/NewAccountActivity.java
@@ -0,0 +1,15 @@
+package com.example.otegoloss;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NewAccountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_account);
+    }
+
+}
