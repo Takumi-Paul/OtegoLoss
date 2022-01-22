f17aa3421854072950bdc8ac2745177d7599c026
Takumi-Paul <230369t@ugs.kochi-tech.ac.jp>
2022-01-22 19:36:56 +0900

    PhotoUploadActivityを作成（標準ギャラリーを呼び出すアクティビティ）

--- app/src/main/java/com/example/otegoloss/PhotoUploadActivity.java
+++ app/src/main/java/com/example/otegoloss/PhotoUploadActivity.java
@@ -17,17 +17,22 @@ public class PhotoUploadActivity extends AppCompatActivity {
     private static final int RESULT_PICK_IMAGEFILE = 1000;
     private ImageView imageView;
 
+    //ImageViewに格納される画像をどうやってFragmentに返そう・・・
+
+    //Activityファイルが呼び出されたら実行される
     protected void onCreate(Bundle savedInstanceState) {
 
+
         super.onCreate(savedInstanceState);
+        //Android標準のギャラリーをよびだす
         Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
         intent.addCategory(Intent.CATEGORY_OPENABLE);
         intent.setType("image/*");
+        //以下の構文が使えないらしい
         startActivityForResult(intent, RESULT_PICK_IMAGEFILE);
     }
 
-
-
+    //アクティビティから返ってきたら以下を実行
     public void onActivityResult(int requestCode, int resultCode, Intent resultData) {
         super.onActivityResult(requestCode, resultCode, resultData);
         if (requestCode == RESULT_PICK_IMAGEFILE && resultCode == RESULT_OK) {
