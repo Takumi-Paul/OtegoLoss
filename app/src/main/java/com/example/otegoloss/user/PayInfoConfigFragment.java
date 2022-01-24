691c941fdf3f60717ae258b52454c027c0f45923
gimmical <230301e@ugs.kochi-tech.ac.jp>
2022-01-24 17:07:29 +0900

    ユーザ画面で、httpのgetを実装　ユーザ・プロフィール設定画面からユーザidの項目削除　ロス削減メーターでhttpのgetを実装

--- app/src/main/java/com/example/otegoloss/user/PayInfoConfigFragment.java
+++ app/src/main/java/com/example/otegoloss/user/PayInfoConfigFragment.java
@@ -1,7 +1,5 @@
 package com.example.otegoloss.user;
 
-import static com.example.otegoloss.user.UserFragment.InputStreamToString;
-
 import android.os.Bundle;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
@@ -46,46 +44,6 @@ public class PayInfoConfigFragment extends Fragment {
         creditcard_company = view.findViewById(R.id.texiview_creditcard_company) ;
         creditcard_number = view.findViewById(R.id.textView_creditcard_number) ;
 
-        // http通信
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    URL url = new URL("http://ec2-13-114-108-27.ap-northeast-1.compute.amazonaws.com/ProductDetails.php?product_id=g0000001");
-                    // 処理開始時刻
-                    startTime = System.currentTimeMillis();
-                    HttpURLConnection con =(HttpURLConnection)url.openConnection();
-                    final String str = InputStreamToString(con.getInputStream());
-
-                    // 終了時刻
-                    endTime = System.currentTimeMillis();
-                    Log.d("HTTP", str);
-
-                    getActivity().runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-                            System.out.println(String.valueOf(str));
-                            System.out.println(endTime - startTime);
-
-                            JSONObject jsnObject = ChangeJson(str);
-                            try {
-                                // Jsonのキーを指定すれば対応する値が入る
-                                //profile_image.setImageBitmap(jsnObject.getString("user_profile_image"));
-                                creditcard_company.setText(jsnObject.getString("card_comp"));
-                                creditcard_number.setText(jsnObject.getString("card_number"));
-                            } catch (JSONException e) {
-                                e.printStackTrace();
-                            }
-
-                        }
-                    });
-                } catch (IOException e) {
-                    e.printStackTrace();
-                    System.out.println(e);
-                }
-            }
-        }).start();
-
         Button addCreditButton = view.findViewById(R.id.addCredit_button);
         Button deleteCreditButton = view.findViewById(R.id.deleteCredit_button);
 
@@ -104,24 +62,6 @@ public class PayInfoConfigFragment extends Fragment {
         });
 
         return view;
-    }
-
-    // Jsonデータに変換
-    static JSONObject ChangeJson(String str) {
-        try {
-            JSONArray jsonArray = new JSONArray(str);
-            // JSONArray jsonArray = jsonObject.getJSONArray("sample");
-            for (int i = 0; i < jsonArray.length(); i++) {
-                JSONObject jsonData = jsonArray.getJSONObject(i);
-                //Log.d("Check", jsonData.getString("user_profile_image"));
-                Log.d("Check", jsonData.getString("card_comp"));
-                Log.d("Check", jsonData.getString("card_number"));
-                return jsonData;
-            }
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-        return null;
     }
 
 }
--- app/src/main/java/com/example/otegoloss/user/ShipingAddrInfoConfigFragment.java
+++ app/src/main/java/com/example/otegoloss/user/ShipingAddrInfoConfigFragment.java
@@ -1,7 +1,5 @@
 package com.example.otegoloss.user;
 
-import static com.example.otegoloss.user.UserFragment.InputStreamToString;
-
 import android.os.Bundle;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
@@ -51,47 +49,6 @@ public class ShipingAddrInfoConfigFragment extends Fragment {
         postalcode_Last = view.findViewById(R.id.textView_postalcode_Last) ;
         user_addr = view.findViewById(R.id.textView_user_addr) ;
 
-        // http通信
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    URL url = new URL("http://ec2-13-114-108-27.ap-northeast-1.compute.amazonaws.com/ProductDetails.php?product_id=g0000001");
-                    // 処理開始時刻
-                    startTime = System.currentTimeMillis();
-                    HttpURLConnection con =(HttpURLConnection)url.openConnection();
-                    final String str = InputStreamToString(con.getInputStream());
-
-                    // 終了時刻
-                    endTime = System.currentTimeMillis();
-                    Log.d("HTTP", str);
-
-                    getActivity().runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-                            System.out.println(String.valueOf(str));
-                            System.out.println(endTime - startTime);
-
-                            JSONObject jsnObject = ChangeJson(str);
-                            try {
-                                // Jsonのキーを指定すれば対応する値が入る
-                                shipping_info_realname.setText(jsnObject.getString("real_name"));
-                                //postalcode_first.setText(jsnObject.getString("card_number"));
-                                //postalcode_Last.setText(jsnObject.getString("card_comp"));
-                                user_addr.setText(jsnObject.getString("address"));
-                            } catch (JSONException e) {
-                                e.printStackTrace();
-                            }
-
-                        }
-                    });
-                } catch (IOException e) {
-                    e.printStackTrace();
-                    System.out.println(e);
-                }
-            }
-        }).start();
-
         Button registerShippingButton = view.findViewById(R.id.registerShipping_button);
         Button deleteShippingButton = view.findViewById(R.id.deleteShipping_button);
 
@@ -110,24 +67,6 @@ public class ShipingAddrInfoConfigFragment extends Fragment {
         });
 
         return view;
-    }
-
-    // Jsonデータに変換
-    static JSONObject ChangeJson(String str) {
-        try {
-            JSONArray jsonArray = new JSONArray(str);
-            // JSONArray jsonArray = jsonObject.getJSONArray("sample");
-            for (int i = 0; i < jsonArray.length(); i++) {
-                JSONObject jsonData = jsonArray.getJSONObject(i);
-                //Log.d("Check", jsonData.getString("user_profile_image"));
-                Log.d("Check", jsonData.getString("real_name"));
-                Log.d("Check", jsonData.getString("address"));
-                return jsonData;
-            }
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-        return null;
     }
 
 }
