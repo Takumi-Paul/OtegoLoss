dfe49a0ff64cff94214fa95fa7296eb316e958b0
Takumi-Paul <230369t@ugs.kochi-tech.ac.jp>
2022-01-30 16:13:10 +0900

    出品履歴画面(未完売)をHTTP通信に対応するよう、コードを追加

--- app/src/main/java/com/example/otegoloss/shipping/ViewYetSoldOutHistoryFragment.java
+++ app/src/main/java/com/example/otegoloss/shipping/ViewYetSoldOutHistoryFragment.java
@@ -1,12 +1,19 @@
 //出品履歴（未完売）
 package com.example.otegoloss.shipping;
 
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Build;
 import android.os.Bundle;
+
+import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 import androidx.navigation.Navigation;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,46 +21,135 @@ import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.GridView;
 
+import com.example.otegoloss.ConnectionJSON;
+import com.example.otegoloss.GridAdapter;
 import com.example.otegoloss.GridAdapterOfShipping;
 import com.example.otegoloss.GridAdapterOfYetSoldOut;
 import com.example.otegoloss.MainActivity;
+import com.example.otegoloss.NewAccountActivity;
 import com.example.otegoloss.R;
-import com.example.otegoloss.user.ProfileConfigFragment;
+import com.example.otegoloss.home.ViewProduct;
 
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.StringJoiner;
+import java.util.stream.Stream;
 
 
 public class ViewYetSoldOutHistoryFragment extends Fragment {
     // 商品名配列
-    private String[] productNames = {
-            "carrot", "radish","tomato"
-    };
+    ProgressDialog progressDialog;
+
+    // http通信の開始・終了時刻
+    long startTime;
+    long endTime;
+
+    // 商品名配列
+    private String[] productNames;
     // 価格配列
-    private int[] prices = {
-            100, 200, 300
-    };
+    private int[] prices;
     // 商品ID
-    private String[] productID = new String[]{
-            "g0000004", "g0000005", "g0000006"
-    };
+    private String[] productID;
     //出品日
-    private String[]  listingDate= new String[]{
-            "2022/01/07", "2021/12/24", "2021/11/10"
-    };
+    private String[]  listingDate;
+
+    // ユーザデータが保存されている変数
+    private SharedPreferences userIDData;
+    // ユーザID
+    String userID;
+
     private List<Integer> imgList = new ArrayList<>();
     //画面表示
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // フラグメントで表示する画面をlayoutファイルからインフレートする
         View view = inflater.inflate(R.layout.fragment_view_yet_sold_out_history, container, false);
+        this.progressDialog   = NewAccountActivity.progressDialog;
 
-        //以下Grid表示についての記述
-        for (String productName: productNames){
-            int imageId = getResources().getIdentifier(productName,"drawable", getActivity().getPackageName());
-            imgList.add(imageId);
+        userIDData = getActivity().getSharedPreferences("DataStore", Context.MODE_PRIVATE);
+        userID = userIDData.getString("userID", "error");
+        System.out.println(userID);
+
+        // http通信
+        Thread t = new Thread(new Runnable() {
+            @RequiresApi(api = Build.VERSION_CODES.N)
+            @Override
+            public void run() {
+                try {
+                    // phpファイルまでのリンク
+                    String path = "http://ec2-13-114-108-27.ap-northeast-1.compute.amazonaws.com/ListingList.php";
+
+                    // クエリ文字列を連想配列に入れる
+                    Map<String, String> map = new HashMap<String, String>();
+                    map.put("user_id", userID);
+                    // クエリ文字列組み立て・URL との連結
+                    StringJoiner stringUrl = new StringJoiner("&", path + "?", "");
+                    for (Map.Entry<String, String> param: map.entrySet()) {
+                        stringUrl.add(param.getKey() + "=" + param.getValue());
+                    }
+                    URL url = new URL(stringUrl.toString());
+
+                    System.out.println(url);
+                    // 処理開始時刻
+                    startTime = System.currentTimeMillis();
+                    HttpURLConnection con = (HttpURLConnection) url.openConnection();
+                    final String str = ConnectionJSON.InputStreamToString(con.getInputStream());
+
+                    // 終了時刻
+                    endTime = System.currentTimeMillis();
+                    Log.d("HTTP", str);
+
+                    getActivity().runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            System.out.println(String.valueOf(str));
+                            System.out.println(endTime - startTime);
+
+                            // Jsonのキーを指定すれば対応する値が入る
+                            //配列の取得
+                            List<String> productNameList = ConnectionJSON.ChangeArrayJSON(str, "product_name");
+                            productNames = productNameList.toArray(new String[productNameList.size()]);
+                            List<String> priceList = ConnectionJSON.ChangeArrayJSON(str, "price");
+                            String[] priceString = priceList.toArray(new String[priceList.size()]);
+                            prices = Stream.of(priceString).mapToInt(Integer::parseInt).toArray();
+                            List<String> produceIDList = ConnectionJSON.ChangeArrayJSON(str, "product_id");
+                            productID = produceIDList.toArray(new String[produceIDList.size()]);
+                            List<String> listingDateList = ConnectionJSON.ChangeArrayJSON(str, "listing_date");
+                            listingDate = listingDateList.toArray(new String[listingDateList.size()]);
+                            List<String> imgStrList = ConnectionJSON.ChangeArrayJSON(str, "product_image");
+
+                            System.out.println("array");
+                            //progressDialog.dismiss();
+                            settingUI(view);
+                        }
+                    });
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    System.out.println(e);
+                }
+            }
+        });
+
+        // ThreadTestクラスの処理が終了するまで待機の指示
+        try {
+            t.start();
+            System.out.println("start");
+            t.join();
+            System.out.println("join");
+        } catch (InterruptedException e) {
+            // 例外処理
+            e.printStackTrace();
         }
+        return view;
+    }
 
+    public void settingUI(View view){
         // GridViewのインスタンスを生成
         GridView gridview = view.findViewById(R.id.grid_view_yet_sold_out_history);
         // BaseAdapter を継承したGridAdapterのインスタンスを生成
@@ -96,6 +192,6 @@ public class ViewYetSoldOutHistoryFragment extends Fragment {
                 Navigation.findNavController(view).navigate(R.id.action_navigation_yet_sold_out_history_to_navigation_shipping);
             }
         });
-        return view;
+
     }
 }
